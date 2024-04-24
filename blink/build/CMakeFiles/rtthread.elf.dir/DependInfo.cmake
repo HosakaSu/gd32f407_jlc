f182a8ca9c48f396bc2dafc1e77d308fceec5378
HosakaSu <liucj1228@outlook.com>
2024-04-24 23:48:21 +0800

    first commit

---
+++ blink/build/CMakeFiles/rtthread.elf.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/mnt/c/Users/liucj/Documents/gd32f407/blink/libraries/CMSIS/GD/GD32F4xx/Source/GCC/startup_gd32f4xx.s" "/mnt/c/Users/liucj/Documents/gd32f407/blink/build/CMakeFiles/rtthread.elf.dir/libraries/CMSIS/GD/GD32F4xx/Source/GCC/startup_gd32f4xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "GD32F407"
+  "USE_STDPERIPH_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../app"
+  "../bsp"
+  "../board"
+  "../bsp/led"
+  "../bsp/uart"
+  "../libraries/CMSIS"
+  "../libraries/CMSIS/GD/GD32F4xx/Include"
+  "../libraries/GD32F4xx_standard_peripheral/Include"
+  "../libraries/GD32F4xx_Firmware_Library/CMSIS/GD/GD32F4xx/Include"
+  "../module/soft_timer"
+  "../module/ringbuffer"
+  "../module/buzzer"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/liucj/Documents/gd32f407/blink/app/main.c" "CMakeFiles/rtthread.elf.dir/app/main.c.obj" "gcc" "CMakeFiles/rtthread.elf.dir/app/main.c.obj.d"
+  "/mnt/c/Users/liucj/Documents/gd32f407/blink/board/board.c" "CMakeFiles/rtthread.elf.dir/board/board.c.obj" "gcc" "CMakeFiles/rtthread.elf.dir/board/board.c.obj.d"
+  "/mnt/c/Users/liucj/Documents/gd32f407/blink/bsp/led/bsp_led.c" "CMakeFiles/rtthread.elf.dir/bsp/led/bsp_led.c.obj" "gcc" "CMakeFiles/rtthread.elf.dir/bsp/led/bsp_led.c.obj.d"
+  "/mnt/c/Users/liucj/Documents/gd32f407/blink/bsp/uart/bsp_uart.c" "CMakeFiles/rtthread.elf.dir/bsp/uart/bsp_uart.c.obj" "gcc" "CMakeFiles/rtthread.elf.dir/bsp/uart/bsp_uart.c.obj.d"
+  "/mnt/c/Users/liucj/Documents/gd32f407/blink/libraries/CMSIS/GD/GD32F4xx/Source/system_gd32f4xx.c" "CMakeFiles/rtthread.elf.dir/libraries/CMSIS/GD/GD32F4xx/Source/system_gd32f4xx.c.obj" "gcc" "CMakeFiles/rtthread.elf.dir/libraries/CMSIS/GD/GD32F4xx/Source/system_gd32f4xx.c.obj.d"
+  "/mnt/c/Users/liucj/Documents/gd32f407/blink/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_adc.c" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_adc.c.obj" "gcc" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_adc.c.obj.d"
+  "/mnt/c/Users/liucj/Documents/gd32f407/blink/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_dma.c" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_dma.c.obj" "gcc" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_dma.c.obj.d"
+  "/mnt/c/Users/liucj/Documents/gd32f407/blink/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_exti.c" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_exti.c.obj" "gcc" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_exti.c.obj.d"
+  "/mnt/c/Users/liucj/Documents/gd32f407/blink/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_gpio.c" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_gpio.c.obj" "gcc" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_gpio.c.obj.d"
+  "/mnt/c/Users/liucj/Documents/gd32f407/blink/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_i2c.c" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_i2c.c.obj" "gcc" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_i2c.c.obj.d"
+  "/mnt/c/Users/liucj/Documents/gd32f407/blink/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_misc.c" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_misc.c.obj" "gcc" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_misc.c.obj.d"
+  "/mnt/c/Users/liucj/Documents/gd32f407/blink/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_rcu.c" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_rcu.c.obj" "gcc" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_rcu.c.obj.d"
+  "/mnt/c/Users/liucj/Documents/gd32f407/blink/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_sdio.c" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_sdio.c.obj" "gcc" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_sdio.c.obj.d"
+  "/mnt/c/Users/liucj/Documents/gd32f407/blink/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_spi.c" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_spi.c.obj" "gcc" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_spi.c.obj.d"
+  "/mnt/c/Users/liucj/Documents/gd32f407/blink/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_syscfg.c" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_syscfg.c.obj" "gcc" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_syscfg.c.obj.d"
+  "/mnt/c/Users/liucj/Documents/gd32f407/blink/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_timer.c" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_timer.c.obj" "gcc" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_timer.c.obj.d"
+  "/mnt/c/Users/liucj/Documents/gd32f407/blink/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_usart.c" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_usart.c.obj" "gcc" "CMakeFiles/rtthread.elf.dir/libraries/GD32F4xx_standard_peripheral/Source/gd32f4xx_usart.c.obj.d"
+  "/mnt/c/Users/liucj/Documents/gd32f407/blink/module/ringbuffer/ringbuffer.c" "CMakeFiles/rtthread.elf.dir/module/ringbuffer/ringbuffer.c.obj" "gcc" "CMakeFiles/rtthread.elf.dir/module/ringbuffer/ringbuffer.c.obj.d"
+  "/mnt/c/Users/liucj/Documents/gd32f407/blink/module/soft_timer/soft_timer.c" "CMakeFiles/rtthread.elf.dir/module/soft_timer/soft_timer.c.obj" "gcc" "CMakeFiles/rtthread.elf.dir/module/soft_timer/soft_timer.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
